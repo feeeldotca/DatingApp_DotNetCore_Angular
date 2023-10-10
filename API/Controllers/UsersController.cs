d2c8d32296123e06c260290a49f8f99e48ea062e
Ken Zhang <feel@shaw.ca>
2023-10-10 02:56:46 -0600

    Chatper 2 almost completed

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+[ApiController]
+[Route("api/[Controller]")]
+public class UsersController : ControllerBase
+{
+  
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;       
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        return _context.Users.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id){
+        return _context.Users.Find(id);
+    }
+
+    [HttpPost]
+    public int UpdateUser(AppUser user){
+        var result = _context.Users.Find(user.Id);
+        if (result==null) return -1;
+        _context.Users.Attach(user);
+        return _context.SaveChanges();
+    }
+
+    [HttpDelete]
+    public int DeleteUserById(int id) {
+        var result = _context.Users.Find(id);
+        if (result==null) return -1;    
+        _context.Users.Remove(result);
+        return _context.SaveChanges();
+    }
+}
+
