32b3c3481fec93132f7cb5fb0018aa82885ac6a5
Kun Zhang <feel@shaw.ca>
2023-10-19 23:26:21 -0600

    Added ITokenService interface / some TokenService

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using System.Data.Common;
 using API.Data;
+using API.Services;
+using API.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,8 +11,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=> opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")) );
 builder.Services.AddCors();
+builder.Services.AddScoped(ITokenService, TokenService);
 var app = builder.Build();
 app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using API.Interface;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
