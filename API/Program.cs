a318fbbf642116c508f8c93bbbaba18289f840da
Kun Zhang <feel@shaw.ca>
2023-10-19 23:29:28 -0600

    little bug fixed in porgram.cs to Add TokenService

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt=> opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")) );
 builder.Services.AddCors();
-builder.Services.AddScoped(ITokenService, TokenService);
+builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
