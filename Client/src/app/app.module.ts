42e7ed81a60a89bd6e3691a83e6fb38c2cb58aed
Kun Zhang <feel@shaw.ca>
2023-10-23 00:06:49 -0600

    Used Ng Forms to replace HTML forms

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule, HttpClient} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    FormsModule
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
