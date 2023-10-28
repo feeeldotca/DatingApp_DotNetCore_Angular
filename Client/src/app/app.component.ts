6151a31ef591b2e68c4f1fe887c6b263755eb623
Kun Zhang <feel@shaw.ca>
2023-10-28 00:09:34 -0600

    added observables to fetch more data

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,28 @@ export class AppComponent implements OnInit {
   title ='Dating App';
   users: any;
   
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private accountService: AccountService){}
 
   ngOnInit(): void {
+    
+      this.getUsers();
+    //throw new Error('Method not implemented.');
+  }
+
+  getUsers(){
     // http.get is an observable need a subscribe otherwise it is lazy
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: res=> this.users = res,
       error: err => console.log(err),
-      complete: ()=> console.log("task completed!")
+      complete: ()=> console.log("Request has completed!")
     });
-    //throw new Error('Method not implemented.');
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
   
 }
